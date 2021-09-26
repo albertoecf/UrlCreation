5884b50d8f4e58754c3eca61e0a26394104d411c
aecf <81099969+albertoecf@users.noreply.github.com>
2021-09-26 16:56:10 -0300

    palabrasExcluir funcion

--- main.py
+++ main.py
@@ -22,19 +22,49 @@ def colsToUse(df1):
 
     return df2
 
+
+palabrasGeneral = [' precios ' , 'precios ', 'precios ', ' precio ', 'precio ' , ' precio' , ' colores ', ' valor ', ' de segunda ' , ' baratos ',' barato ', ' economicos ' ,' y ']
+
 def cleanKw(df):
     '''Nos permite limpiar los terminos de busqueda y prepararlos para la url'''
-    #Limpiamos caracteres relacionados al matchType
+    #caracteres matchtype
     replaceList = ['"','+', '[', ']']
+    #Palabras a excluir
+    
+   
     df1 = df.copy()
+   
+    df1['kwNoMatchType'] = df1['kwCopy'].copy()
+   
+    #Limpiamos caracteres relacionados al matchType
     for i in replaceList:
-        df1['kwCopy'] = df1['kwCopy'].str.replace(i, '')
+        df1['kwNoMatchType'] = df1['kwNoMatchType'].str.replace(i, '')
+
+    df1['kwNoGeneralList'] = df1['kwNoMatchType'].copy()
+
+    #Limpiamos palabras Generales
+    for j in palabrasGeneral:
+        df1['kwNoGeneralList'] = df1['kwNoGeneralList'].str.replace(j, ' ')
+    
     # Borramos espacios en blanco en ambos lados 
-    df1['kwCopy'] = df1['kwCopy'].str.strip()
+    df1['kwClean'] = df1['kwNoGeneralList'].str.strip()
     # Remplazamos el espacio que separa por guión medio 
-    df1['kwCopy'] = df1['kwCopy'].str.replace(' ','-')
+    df1['kwCleanSlash'] = df1['kwClean'].str.replace(' ','-')
     return df1
 
+
+def armarCombinaciones(listaPalabras):
+    '''Nos permite armar combinaciones de palabras agregando espacios antes, después, ambos y palabra original'''
+    combinacionesPalabras = []
+    for pal in listaPalabras: 
+        nuevaCombinacion = " " + pal + " "
+        combinacionesPalabras.append(nuevaCombinacion)
+        nuevaCombinacion = " " +  pal 
+        combinacionesPalabras.append(nuevaCombinacion)
+        nuevaCombinacion = pal + " "
+        combinacionesPalabras.append(nuevaCombinacion)
+        combinacionesPalabras.append(pal)
+    return combinacionesPalabras
 
 
 #%%
@@ -48,7 +78,42 @@ reducidoLimpio = cleanKw(reducido)
 
 reducidoLimpio.head()   
 
+
+
+
+UrlPais = 'https://listado.mercadolibre.com.ar/'
+
+reducidoLimpio['preUrl'] = UrlPais
+
+reducidoLimpio['urlArmada'] = reducidoLimpio['preUrl'] + reducidoLimpio['kwCopy']
 # %%
-print(reducido.head())
-reducido.info()
+
+# %%
+
+palabrasExcluir = ['precio', 'barato', 'peru']
+
+def armarCombinaciones(listaPalabras):
+    combinacionesPalabras = []
+    for pal in listaPalabras: 
+        nuevaCombinacion = " " + pal + " "
+        combinacionesPalabras.append(nuevaCombinacion)
+        nuevaCombinacion = " " +  pal 
+        combinacionesPalabras.append(nuevaCombinacion)
+        nuevaCombinacion = pal + " "
+        combinacionesPalabras.append(nuevaCombinacion)
+        combinacionesPalabras.append(pal)
+    return combinacionesPalabras
+
+
+combinacionesExcluir = armarCombinaciones(palabrasExcluir)
+
+print(combinacionesExcluir)
+
+
+# %%
+
+# %%
+
+# %%
+
 # %%
